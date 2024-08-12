8833dfed552db26997a8327264a31d7aafe1e1fe
CanSamet0 <can_samet00@outlook.com>
2024-08-12 15:22:49 +0300

    Splash screen added and home page design made.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,22 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.core.splashscreen)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
